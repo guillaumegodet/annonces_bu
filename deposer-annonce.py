23b4b265bfb55f670bee72ef6132295cc47d6814
guillaumegodet <guillaumegodet@gmail.com>
2025-04-10 12:12:17 +0200

    Update deposer-annonce.py

--- deposer-annonce.py
+++ deposer-annonce.py
@@ -50,11 +50,11 @@ def upload_to_drive(file_path, folder_id):
 FOLDER_ID = '1_2InKzKskt1q-0S9HFWXlap380QF4hi2'
 
 # Titre et description
-st.title("Une nouvelle annonce sonore pour la BU Droit !")
+st.title("📢 Une nouvelle annonce sonore pour la BU Droit !")
 st.write("""> La bibliothèque ferme dans 30 minutes. A votre départ, merci de repousser votre chaise et de laisser votre place propre. Nous serons heureux de vous accueillir demain matin à partir de 9h et jusqu'à 13h. Bonne soirée.
 
 Si vous fréquentez la BU le soir vers 18h30, vous avez l'habitude d'entendre ce type de message. Et si vous faisiez entendre votre voix en renouvellement ces annonces sonores ?  
-Enregistrez vos annonces, transmettez-nous les fichiers et vous aurez peut-être la chance d'entendre votre belle voix résonner le soir dans toute la BU !""")
+Enregistrez vos annonces ici et vous aurez peut-être la chance d'entendre votre belle voix résonner le soir dans toute la BU !""")
 
 # Fonction générique pour un formulaire d'enregistrement
 def enregistrer_annonce(titre_formulaire, nom_fichier, texte_annonce):
